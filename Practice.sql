9746404b73a485d317dfde44edf849d5c979deb6
Gajanand-L-I <gajanandimmannavar@gmail.com>
2026-07-10 22:15:10 +0530

    Added Practice Queries

--- Practice.sql
+++ Practice.sql
@@ -99,3 +99,119 @@ where Dept in ('aiml','cs');
 
 select Name, max(Age) as Sceiner FROM StudentInfo;
 
+
+
+create table Hi (Id int,
+Name char,
+Dept char,
+Age int,
+marks int,
+phone int
+);
+
+desc hi;
+
+alter table Hi
+add Dob date;
+
+alter table hi modify
+dob int;
+
+alter table hi modify
+dob date;
+
+
+alter table hi  rename  to Students;
+
+select * from Students
+
+desc students;
+
+alter table students change  dob to Dob;
+
+desc students;
+
+alter table students modify dob int;
+
+alter table students modify dob date;
+
+desc students;
+
+alter table students 
+drop dob;
+
+desc students;
+
+alter table students add Dob date;
+
+desc students;
+
+
+insert INTO students(
+    id, name, dept, age, marks, phone, dob
+)values(
+    01,'Gajanand', 'Aiml', 22, 100, 1234567890,'15-07-2004'
+);
+
+select * from Students;
+
+update students set Name = 'Gajanand' where Age = 22;
+
+update students set age = 21 WHERE name = 'G';
+
+alter table students modify  id int primary key AUTO_INCREMENT
+
+insert into students (name, dept, age, marks, phone, dob)values(
+    'Mallu', 'Aiml', 22, 100, 1223344512, '18-05-2004'
+);
+
+select * from Students;
+
+desc Students;
+
+alter table Students modify Name Char(20);
+
+alter table students modify dept char(20), modify Dob DATE;
+
+insert into students ( name, dept, age, marks, phone,Dob)value(
+    'Akash','IS', 22, 100, 8134589375, '15-07-2004'
+);
+
+select  * from Students;
+
+update students set Dob = '2004-07-15'
+where id = 1;
+
+select * from Students;
+
+
+select * from students 
+where Name like '_____'
+
+select * from students
+where age like '2_'
+
+
+select * from  students 
+
+where name like '%G' and  age =22;
+
+select * from students where age BETWEEN 20 and 21;
+
+select * from Students where age between 21 and 22;
+
+update Students SET dept = 'Aiml' WHERE id =1;
+
+update Students SET dept = 'aiml' where id = 2;
+
+select * from Students ;
+
+update students set Name = 'Gajanand' where dept = 'Aiml';
+
+
+select * from students;
+
+
+update students SET name = 'Chandru' where id = 2;
+
+select * from Students;
