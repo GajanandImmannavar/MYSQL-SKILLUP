bed52a9bfcdae9d42a617168d0a116326805dc21
Gajanand-L-I <gajanandimmannavar@gmail.com>
2026-07-30 11:46:30 +0530

    Add 19th Day of Journey of Mysql (Inner Join)

---
+++ Day-19.sql
@@ -0,0 +1,39 @@
+use Joins;
+
+-- show Tables;
+
+-- select * from Orders;
+
+-- 🏢 Main Problem
+-- Business Scenario
+
+-- The sales director wants to identify valuable customers who purchased premium products.
+
+-- Prepare a report showing:
+
+-- Customer Name
+-- Product Name
+-- Price
+-- Payment Method
+
+-- Include only those orders where:
+
+-- Product is AVAILABLE
+-- Payment Status is PAID
+-- Product price is greater than ₹30,000
+-- Customer belongs to BENGALURU or MYSURU
+-- Brand name does not start with 'A'
+
+-- Sort the report by:
+
+-- Customer Name (A → Z)
+-- Price (Highest → Lowest)
+-- 📚 Concepts Covered
+-- ✅ INNER JOIN (3 Tables)
+-- ✅ WHERE
+-- ✅ IN
+-- ✅ NOT LIKE
+-- ✅ ORDER BY
+
+-- select Customer.Customer_Name, Inventory.Product_Name, Inventory.Price, Orders.Payment_Method
+-- from customer  
