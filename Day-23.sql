c9fe7cf565cd9ab2be1e4ae3bbd2d0c514f82876
GajanandImmannavar <gajanandimmannavar@gmail.com>
2026-08-18 01:30:59 +0530

    Add Problems on Left join and more conditions using and

--- Day-23.sql
+++ Day-23.sql
@@ -29,6 +29,119 @@
 
 -- "Only products that are AVAILABLE."
 
+-- select  Inventory.Product_Name, 
+--         Inventory.Price, 
+--         Inventory.Product_Status, 
+--         Supplier.Supplier_City, 
+--         Supplier.Supplier_Name
+-- from Supplier
+
+-- LEFT JOIN Inventory
+--     on Supplier.Supplier_Id = Inventory.Supplier_Id
+--     and Inventory.Product_Status = "AVAILABLE"
+--     ORDER BY Supplier.Supplier_Name asc, 
+--     Inventory.Price desc;
 
 
+-- The customer support manager wants to see all customers and their orders.
 
+-- Display:
+
+-- Customer Name
+-- Order ID
+-- Payment Status
+-- Order Date
+
+-- Requirements:
+
+-- Show every customer.
+-- Show only PAID orders.
+-- Customers without PAID orders must still appear.
+-- Sort by Customer Name A → Z.
+
+
+-- select Customer.Customer_Name, Orders.Order_Id,
+--        Orders.Payment_Status,
+--        Orders.Order_Date
+-- from Customer 
+-- left join Orders
+--         on Customer.Customer_Id = Orders.Customer_Id
+--         and Orders.Payment_Status = "PAID"
+-- order by Customer.Customer_Name asc;
+
+-- inside the ON condition.
+
+-- Customer
+--    │
+--    │ LEFT JOIN
+--    ▼
+-- Orders
+--    │
+--    ├── PAID     → MATCH ✅
+--    ├── PENDING  → NULL
+--    ├── FAILED   → NULL
+--    └── No order → NULL
+
+
+-- The category manager wants a report containing:
+
+-- Category Name
+-- Product Name
+-- Brand
+-- Price
+
+-- Requirements:
+
+-- Show every category.
+-- Show only products with:
+-- Rating ≥ 4
+-- Price > ₹20,000
+-- Product Status = AVAILABLE
+-- Categories without matching products must still appear.
+
+-- Sort by:
+
+-- Category Name A → Z
+-- Price highest → lowest
+
+select Categories.Category_Name,
+       Inventory.Product_Name, 
+       Inventory.Brand,
+       Inventory.Price
+from Categories
+left join Inventory
+    on Categories.Category_ID = Inventory.Category_Id
+    and Inventory.Rating >=4
+    and Inventory.Price > 20000
+    and Inventory.Product_Status = 'AVAILABLE'
+order by Categories.Category_Name asc,
+         Inventory.Price desc;
+
+
+-- Think:
+
+-- Categories
+--     │
+--     │ LEFT JOIN
+--     ▼
+-- Inventory
+--     │
+--     ├── Rating ≥ 4
+--     │
+--     ├── Price > 20,000
+--     │
+--     └── AVAILABLE
+--            ↓
+--         MATCH ✅
+
+-- If a category has no product satisfying all three conditions:
+
+-- Category
+--    │
+--    └── No matching product
+--             ↓
+--      Product_Name → NULL
+--      Brand        → NULL
+--      Price        → NULL
+
+-- The category still appears. ✅
