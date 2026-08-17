5681b4c7706f3b353b0711c0564c76898f297da3
GajanandImmannavar <gajanandimmannavar@gmail.com>
2026-08-18 00:01:16 +0530

    Add Problems on Left join

---
+++ Day-23.sql
@@ -0,0 +1,34 @@
+-- use Joins;
+
+-- show tables;
+
+-- The inventory manager wants to review all suppliers and their products.
+
+-- Prepare a report showing:
+
+-- Supplier Name
+-- Supplier City
+-- Product Name
+-- Product Price
+-- Product Status
+-- Requirements
+-- Show every supplier, even if they have no products.
+-- If a supplier has products, show only products that are currently AVAILABLE.
+-- Sort by:
+-- Supplier Name (A → Z)
+-- Product Price (Highest → Lowest)
+-- ⚠️ Important
+
+-- The phrase:
+
+-- "Show every supplier, even if they have no products."
+
+-- means you must preserve all suppliers.
+
+-- And:
+
+-- "Only products that are AVAILABLE."
+
+
+
+
